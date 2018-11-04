3d7649e308e8314589c93ae84300ac359aa424c0
e <444903412@qq.com>
2018-11-05 02:02:33 +0800

    第一次更新

---
+++ webpack-vue/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <div>这是首页</div>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style scoped>
+
+</style>
